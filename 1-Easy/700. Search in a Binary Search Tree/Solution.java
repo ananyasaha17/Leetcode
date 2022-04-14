908ef6c056c5fd0cf35a397ddcfb3ea76b95c413
Ananya Saha <72212849+ananyasaha17@users.noreply.github.com>
2022-04-14 22:07:03 +0530

    day 16

---
+++ 1-Easy/700. Search in a Binary Search Tree/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null){
+            if(root.val == val){
+                return root;
+            } else if(root.val > val){
+                root = root.left;
+            } else if(root.val < val){
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
